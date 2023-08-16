d906ff3a260485aa08225df7a517a8e5e0de875c
soma inagaki <soma@somanoMacBook-Air.local>
2023-08-16 17:43:00 +0900

    a

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,12 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
-
+require "minitest/reporters"
+Minitest::Reporters.use!
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+# 指定のワーカー数でテストを並列実行する 
+parallelize(workers: :number_of_processors)
+# test/fixtures/*.yml にあるすべての fixture をセットアップする 
+fixtures :all
+# (すべてのテストで使うその他のヘルパーメソッドは省略) 
 end
